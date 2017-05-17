4a307f5544fbb7f716b3f51daccc6d461faead55
nkumar8421 <nkumar8421@gmail.com>
2017-05-17 13:54:36 +0530

    Add files via upload

---
+++ CalcHelper/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcHelper
+{
+    public class Class1
+    {
+        public static int SumOfTwoInt(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
