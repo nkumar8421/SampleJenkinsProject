ead0099c4a79f4d84c19c45b15e1a2ad652e4801
nkumar8421 <nkumar8421@gmail.com>
2017-05-17 13:56:58 +0530

    Add files via upload

---
+++ Calculator/Calculator.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CalcHelper;
+namespace Calculator
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            TextBox3.Text= CalcHelper.Class1.SumOfTwoInt(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+    }
+}
