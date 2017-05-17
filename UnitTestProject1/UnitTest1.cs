8b7ead2d9a51ca3360bd8e7ffdd817acd0681761
nkumar8421 <nkumar8421@gmail.com>
2017-05-17 18:07:57 +0530

    Add files via upload

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           Assert.AreEqual(20, CalcHelper.Class1.SumOfTwoInt(10, 10), "Sum of two integers is wrong");
+        }
+    }
+}
